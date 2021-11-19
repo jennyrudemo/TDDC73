3bd612e68bdb5e386aa6a26b0df35926a68bf5f2
Jenny Rudemo <jenru723@student.liu.se>
2021-11-19 14:28:27 +0100

    Start of lab 2

---
+++ Lab 2/flutter_lab2_interaction/lib/main.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/credit_card_brand.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Credit card interaction'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String cardNumber = '';
+  String expiryDate = '';
+  String cardHolderName = '';
+  String cvvCode = '';
+  bool isCvvFocused = false;
+  bool useGlassMorphism = false;
+  bool useBackgroundImage = false;
+  OutlineInputBorder? border;
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.grey.withOpacity(0.7),
+        width: 2.0,
+      ),
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CreditCardWidget(
+              cardNumber: cardNumber,
+              expiryDate: expiryDate,
+              cardHolderName: cardHolderName,
+              cvvCode: cvvCode,
+              showBackView:
+                  isCvvFocused, //true when you want to show cvv(back) view
+              //backgroundImage: 'assets/card_bg.png',
+              //obscureCardNumber: false,
+              isHolderNameVisible: true,
+              isChipVisible: true,
+              onCreditCardWidgetChange: (CreditCardBrand creditCardBrand) {},
+            ),
+            CreditCardForm(
+              cardHolderName: cardHolderName,
+              expiryDate: expiryDate,
+              cardNumber: cardNumber,
+              cvvCode: cvvCode,
+              formKey: formKey, // Required
+              onCreditCardModelChange: (CreditCardModel data) {}, // Required
+              themeColor: Colors.red,
+              obscureCvv: true,
+              obscureNumber: true,
+              isHolderNameVisible: true,
+              isCardNumberVisible: true,
+              isExpiryDateVisible: true,
+              cardNumberDecoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Number',
+                hintText: 'XXXX XXXX XXXX XXXX',
+              ),
+              expiryDateDecoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Expired Date',
+                hintText: 'XX/XX',
+              ),
+              cvvCodeDecoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'CVV',
+                hintText: 'XXX',
+              ),
+              cardHolderDecoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Card Holder',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
