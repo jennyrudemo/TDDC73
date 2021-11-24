dffea74df3544cb9f6f48e81e7b1c5548e88cbfc
Richard Gotthard <richard.gotthard@hotmail.com>
2021-11-24 10:54:28 +0100

    Uppdaterad

--- Lab 3/flutter_application_1/lib/main.dart
+++ Lab 3/flutter_application_1/lib/main.dart
@@ -1,7 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'fetchmore/main.dart';
 
-void main() {
+String get host {
+  if (Platform.isAndroid) {
+    return '10.0.2.2';
+  } else {
+    return 'localhost';
+  }
+}
+
+void main() async {
+  await initHiveForFlutter();
+
+  final HttpLink httpLink = HttpLink(
+    'https://api.github.com/graphql',
+  );
+
+  final AuthLink authLink = AuthLink(
+    getToken: () async =>
+        'Bearer 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', //ändra denna från token
+    // OR
+    // getToken: () => 'Bearer <YOUR_PERSONAL_ACCESS_TOKEN>',
+  );
+
+  final Link link = authLink.concat(httpLink);
+
+  ValueNotifier<GraphQLClient> client = ValueNotifier(
+    GraphQLClient(
+      link: link,
+      // The default store is the InMemoryStore, which does NOT persist to disk
+      cache: GraphQLCache(store: HiveStore()), //Från store till cache
+    ),
+  );
+
   runApp(const MyApp());
 }
 
@@ -11,12 +43,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return GraphQLProvider(
+      //client: client,
+      //linking fungerar ej
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(title: 'Lab 3'),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -31,14 +67,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,23 +74,40 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: Query(
+          options: QueryOptions(
+            document: gql(
+                readRepositories), // this is the query string you just created
+            variables: {
+              'nRepositories': 50,
+            },
+            pollInterval: Duration(seconds: 10),
+          ),
+
+          // Just like in apollo refetch() could be used to manually trigger a refetch
+          // while fetchMore() can be used for pagination purpose
+          builder: (QueryResult result,
+              {VoidCallback refetch, FetchMore fetchMore}) {
+            if (result.hasException) {
+              return Text(result.exception.toString());
+            }
+
+            if (result.isLoading) {
+              return Text('Loading');
+            }
+
+            // it can be either Map or List
+            List repositories = result.data['viewer']['repositories']['nodes'];
+
+            return ListView.builder(
+                itemCount: repositories.length,
+                itemBuilder: (context, index) {
+                  final repository = repositories[index];
+
+                  return Text(repository['name']);
+                });
+          },
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
