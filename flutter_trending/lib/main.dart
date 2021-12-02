8814a64c78b9c2fd9ac2ca1a096ea969339ac322
Jenny Rudemo <jenru723@student.liu.se>
2021-12-02 17:37:42 +0100

    Detailed info for repos and possibility to switch language

--- flutter_trending/lib/main.dart
+++ flutter_trending/lib/main.dart
@@ -1,8 +1,5 @@
-//import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
-import 'package:dio/dio.dart';
 
 // void getHttp() async {
 //   try {
@@ -25,7 +22,7 @@ void main() async {
 
   final AuthLink authLink = AuthLink(
     getToken: () async =>
-        'Bearer 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', //ändra denna från token
+        'Bearer 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', //ändra denna från token
     // OR
     // getToken: () => 'Bearer <YOUR_PERSONAL_ACCESS_TOKEN>',
   );
@@ -67,51 +64,109 @@ class MyApp extends StatelessWidget {
   }
 }
 
-//Måste vara stateful widget?
-/*class DetailedInfo extends StatelessWidget {
-  var title;
-
-  /*dynamic repository;
-
+class DetailedInfo extends StatelessWidget {
   DetailedInfo({
-    this.repository,
+    Key? key,
+    required this.title,
+    required this.description,
+    required this.licens,
+    required this.commits,
+    required this.branches,
+  }) : super(key: key);
 
-  })*/
-  
-  DetailedInfo({
-      this.title,
+  String title = "";
+  String description = "";
+  String licens = "";
+  int commits = 0;
+  int branches = 0;
 
-  })
-  
+  @override
+  //State<StatelessWidget> createState() => _DetailedInfo();
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey.shade800,
+        appBar: AppBar(
+          title: const Text('Lab 3'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              title,
+              style: Theme.of(context).textTheme.headline3,
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(5.0, 20.0, 5.0, 20.0),
+              child: Text(
+                description,
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.headline5,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Licens",
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    Text(
+                      "Commits",
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    Text(
+                      "Branches",
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      licens,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    Text(
+                      commits.toString(),
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    Text(
+                      branches.toString(),
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                  ],
+                ),
+              ],
+            )
+            /*Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [const Text("Licens"), Text(licens)],
+            ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [const Text("Commits"), Text(commits.toString())]),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [const Text("branches"), Text(branches.toString())]),*/
+          ],
+        ));
+  }
+}
 
-  
+/*class _DetailedInfo extends State<DetailedInfo> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [],
+    return Container(
+      color: Colors.white,
+      child: Text(widget.title),
     );
   }
 }*/
-
-class DetailedInfo extends StatefulWidget {
-
-  DetailedInfo({
-    this.title,
-  })
-
-  String title = "";
-
-  @override
-  State<StatefulWidget> createState() => _DetailedInfo();
-}
-
-class _DetailedInfo extends State<DetailedInfo> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -181,6 +236,10 @@ class _MyHomePageState extends State<MyHomePage> {
             MaterialPageRoute(
                 builder: (context) => DetailedInfo(
                       title: repository['name'],
+                      description: repository['description'],
+                      licens: repository['licenseInfo']['name'],
+                      commits: repository['object']['history']['totalCount'],
+                      branches: repository['refs']['totalCount'],
                     )));
       },
       child: Container(
@@ -221,6 +280,24 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  Widget dropDown() {
+    //String dropDownValue = "Python";
+    return DropdownButton<String>(
+      onChanged: (String? newValue) {
+        _setSelectedLanguage(newValue!);
+      },
+      value: selectedLanguage,
+      //Bakgrundsfärg så att den vita texten går att läsa
+      dropdownColor: Colors.grey,
+      items: languageList.map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -229,51 +306,57 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Query(
-          options: QueryOptions(
-            document: gql(
-                readRepositories), // this is the query string you just created
-            variables: {
-              'nRepositories':
-                  50, //querystring : 'language $selectedlanguage stars: >1000
-              'queryString':
-                  'sort:stars-desc language: $selectedLanguage stars:>1000',
-            },
-            pollInterval: const Duration(seconds: 10),
-          ),
-          // Just like in apollo refetch() could be used to manually trigger a refetch
-          // while fetchMore() can be used for pagination purpose
-          builder: (QueryResult result,
-              {FetchMore? fetchMore, VoidCallback? refetch}) {
-            if (result.hasException) {
-              return Text(result.exception.toString());
-            }
+        child: Column(
+          children: [
+            //Lägger till Expanded för att undvika problem med att query/listview expanderar för mycket (?)
+            Expanded(
+              child: Query(
+                options: QueryOptions(
+                  document: gql(
+                      readRepositories), // this is the query string you just created
+                  variables: {
+                    'nRepositories':
+                        50, //querystring : 'language $selectedlanguage stars: >1000
+                    'queryString':
+                        'sort:stars-desc language: $selectedLanguage stars:>1000',
+                  },
+                  pollInterval: const Duration(seconds: 10),
+                ),
+                // Just like in apollo refetch() could be used to manually trigger a refetch
+                // while fetchMore() can be used for pagination purpose
+                builder: (QueryResult result,
+                    {FetchMore? fetchMore, VoidCallback? refetch}) {
+                  if (result.hasException) {
+                    return Text(result.exception.toString());
+                  }
 
-            if (result.isLoading) {
-              return Text('Loading');
-            }
+                  if (result.isLoading) {
+                    return Text('Loading');
+                  }
 
-            // it can be either Map or List
-            List repositories = result.data?['search']['nodes'];
+                  // it can be either Map or List
+                  List repositories = result.data?['search']['nodes'];
 
-            return ListView.builder(
-                itemCount: repositories.length,
-                itemBuilder: (context, index) {
-                  final repository = repositories[index];
+                  return ListView.builder(
+                      itemCount: repositories.length,
+                      itemBuilder: (context, index) {
+                        final repository = repositories[index];
 
-                  /*return Text(repository['name'] +
-                      " " +
-                      repository['stargazers']['totalCount'].toString());
-                  */
-                  return box(repository);
-                  //repository['stargazers: totalCount']);
-                  //här kan man returnera ett kort genom card.dart
-                });
-          },
+                        /*return Text(repository['name'] +
+                          " " +
+                          repository['stargazers']['totalCount'].toString());
+                      */
+                        return box(repository);
+                        //repository['stargazers: totalCount']);
+                        //här kan man returnera ett kort genom card.dart
+                      });
+                },
+              ),
+            ),
+            dropDown(),
+          ],
         ),
       ),
-
-      //lägga till en container
     );
   }
 }
