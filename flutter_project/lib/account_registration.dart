fb2f726ed551cd0491458f64fef9bbc3d9d99aae
Jenny Rudemo <jennyrudemo@gmail.com>
2021-12-08 09:55:07 +0100

    Start of account registration widget

---
+++ flutter_project/lib/account_registration.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+TextField firstName() {
+  return TextField();
+}
+
+Widget accountRegistration() {
+  //TODO: replace with actual content
+  return Container(
+    color: Colors.amber,
+    child: firstName(),
+  );
+}
