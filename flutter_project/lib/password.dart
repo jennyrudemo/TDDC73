93e3e782107eb089fde46570c0509728bededf3f
Richard Gotthard <richard.gotthard@hotmail.com>
2021-12-08 12:16:10 +0100

    Uppdaterat password

--- flutter_project/lib/password.dart
+++ flutter_project/lib/password.dart
@@ -1,12 +1,5 @@
 import 'package:flutter/material.dart';
 
-bool validateStructure(String value) {
-  String pattern =
-      r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
-  RegExp regExp = new RegExp(pattern);
-  return regExp.hasMatch(value);
-}
-
 class PasswordForm extends StatefulWidget {
   const PasswordForm({Key? key}) : super(key: key);
 
@@ -17,8 +10,15 @@ class PasswordForm extends StatefulWidget {
 }
 
 class PasswordFormState extends State<PasswordForm> {
-  //var _usernameController = TextEditingController();
-  //String _usernameError;
+  bool validateStructure(String value) {
+    String pattern =
+        r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
+    RegExp regExp = RegExp(pattern);
+    return regExp.hasMatch(value);
+  }
+
+  var passwordController = TextEditingController();
+  String passwordError = "";
 
   @override
   Widget build(BuildContext context) {
@@ -28,26 +28,26 @@ class PasswordFormState extends State<PasswordForm> {
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
           child: TextFormField(
-            // controller: _usernameController,
+            controller: passwordController,
+            //onChanged: validate(),
             obscureText: true,
             enableSuggestions: false,
             autocorrect: false,
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'Enter your password',
-            ),
+            decoration: InputDecoration(
+                labelText: 'Enter your password',
+                hintText: "Password",
+                errorText: passwordError),
           ),
         ),
       ],
     );
   }
-  /*validate(){
-        if(!validateStructure(_usernameController.text)){
-            setState(() {
-                _usernameError = emailError;
-                _passwordError = passwordError;
-            });
-            // show dialog/snackbar to get user attention.
-            return;
-        }*/
+
+  // validate() {
+  //   if (!validateStructure(passwordController.text)) {
+  //     // show dialog/snackbar to get user attention.
+
+  //     return;
+  //   }
+  // }
 }
